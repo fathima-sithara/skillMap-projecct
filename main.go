42bc2194debeec3b83ed79c60320924df6724924
fathima sithara <fathimasiithara@gmail.com>
2025-03-15 21:19:01 +0530

    SP-4 completed first API

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("skill map project")
+
+	r := gin.Default()
+	r.GET("/home", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "skill-map-home-page ",
+		})
+	})
+	r.Run()
+}
